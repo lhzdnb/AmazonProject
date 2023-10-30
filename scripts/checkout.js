532e9a8c00f21d70bef4660253a0165c74ec4ea0
lh20020727 <lh20020727@sjtu.edu.cn>
2023-10-29 21:29:19 -0700

    fix price formatting and radio selectors

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,7 @@
 import {cart} from '../data/cart.js';
 import {products} from '../data/products.js';
+import {formatCurrency} from "./utils/money.js";
+
 let cartSummaryHTML = '';
 cart.forEach((cartItem) => {
     const product = products.find((product) => {
@@ -20,7 +22,7 @@ cart.forEach((cartItem) => {
                   ${product.name}
                 </div>
                 <div class="product-price">
-                  $${(product.priceCents / 100).toFixed(2)}
+                  $${formatCurrency(product.priceCents)}
                 </div>
                 <div class="product-quantity">
                   <span>
@@ -42,7 +44,7 @@ cart.forEach((cartItem) => {
                 <div class="delivery-option">
                   <input type="radio" checked
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${product.id}">
                   <div>
                     <div class="delivery-option-date">
                       Tuesday, June 21
@@ -55,7 +57,7 @@ cart.forEach((cartItem) => {
                 <div class="delivery-option">
                   <input type="radio"
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${product.id}">
                   <div>
                     <div class="delivery-option-date">
                       Wednesday, June 15
@@ -68,7 +70,7 @@ cart.forEach((cartItem) => {
                 <div class="delivery-option">
                   <input type="radio"
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${product.id}">
                   <div>
                     <div class="delivery-option-date">
                       Monday, June 13
@@ -84,4 +86,3 @@ cart.forEach((cartItem) => {
 });
 
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
-console.log(cartSummaryHTML);
