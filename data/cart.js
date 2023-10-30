706ab5e05ddb0bebd2b424c45354c44a9edd6ead
lh20020727 <lh20020727@sjtu.edu.cn>
2023-10-29 19:34:06 -0700

    move addToCart into cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,18 @@
 export const cart = [];
+
+export function addToCart(productId) {
+    let matchingItem = null;
+    cart.forEach((cartItem) => {
+        if (cartItem.productId === productId) {
+            matchingItem = cartItem;
+        }
+    });
+    if (matchingItem) {
+        matchingItem.quantity += 1;
+    } else {
+        cart.push({
+            productId: productId,
+            quantity: 1,
+        });
+    }
+}
