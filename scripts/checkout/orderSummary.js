a0c40e0e1cdd7b9e8728b8a71f4c705e76ecd8e4
lh20020727 <lh20020727@sjtu.edu.cn>
2023-11-05 16:13:06 -0800

    Save data and generate HTML for payment summary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -1,7 +1,7 @@
 import {cart, removeFromCart, calculateCartQuantity, updateQuantity, updateDeliveryOption} from '../../data/cart.js';
 import {products} from '../../data/products.js';
 import formatCurrency from "../utils/money.js"; // default export
-import deliveryOptions from "../../data/deliveryOptions.js";
+import {deliveryOptions, getDeliveryOption} from "../../data/deliveryOptions.js";
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 
 export function renderOrderSummary() {
@@ -13,12 +13,7 @@ export function renderOrderSummary() {
         });
 
         const deliveryOptionId = cartItem.deliveryOptionId;
-        let deliveryOption;
-        deliveryOptions.forEach((option) => {
-            if (option.id === deliveryOptionId) {
-                deliveryOption = option;
-            }
-        });
+        const deliveryOption = getDeliveryOption(deliveryOptionId);
 
         const today = dayjs();
         const deliveryDate = today.add(deliveryOption.deliveryDays, 'days');
